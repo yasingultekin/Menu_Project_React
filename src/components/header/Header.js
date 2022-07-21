8a2371ded6321721c80c4d033b339ec0636303a7
yasingultekin <yasingultekin13@gmail.com>
2022-07-21 15:19:40 +0300

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-container">
+      <h1>Our Menu</h1>
+      <div className="underline"></div>
+    </div>
+  );
 };
 
 export default Header;
