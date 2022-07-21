c6a74648210c7ab5ea14e234cf4cb0de3875f5d9
yasingultekin <yasingultekin13@gmail.com>
2022-07-21 15:34:56 +0300

    menu img

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Menu.css";
 
 const Menu = ({ menuItems }) => {
-  return <div className="main-container"></div>;
+  return (
+    <div className="main-container">
+      {menuItems.map((item) => {
+        const { id, img, desc, price, title } = item;
+        return (
+          <div className="menu-container">
+            <img src={img} alt="" />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
