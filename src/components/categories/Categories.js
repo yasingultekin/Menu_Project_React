2a8180b74398f94f2a6126a9abcfd68dc5683e7c
yasingultekin <yasingultekin13@gmail.com>
2022-07-21 15:25:18 +0300

    categories

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Categories.css";
 
-const Categories = () => {
-  return <div>Categories</div>;
+const Categories = ({ allCategories }) => {
+  return (
+    <div className="btn-container">
+      {allCategories.map((categoryItem, index) => {
+        return (
+          <button className="category-button" key={index}>
+            {categoryItem}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 import Categories from "../components/categories/Categories";
 import Header from "../components/header/Header";
 import Menus from "../components/menus/Menus";
+import data from "../helper/data";
+
+const allCategories = ["all", ...new Set(data.map((item) => item.category))];
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Categories />
+      <Categories allCategories={allCategories} />
       <Menus />
     </div>
   );
